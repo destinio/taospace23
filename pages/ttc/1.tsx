79151521f3a653c411694e1e2304d748cb59c4ea
Destin Lee <dleeinc@gmail.com>
2023-01-02 13:40:10 -0600

    firsdt

---
+++ pages/ttc/1.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export default function one() {
+  return (
+    <>
+      <h2>One: The Tao</h2>
+      <section>
+        <p>The Tao that can be named to not the true Tao.</p>
+        <p>Having a name. It is the creator or all things.</p>
+        <br />
+        <p>Live without desire.</p>
+        <p>To live with desire is to only see the named.</p>
+        <br />
+        <p>Name and unnamed. Together we call it the Mystery.</p>
+        <p>The Mystery is the gate to all that is.</p>
+      </section>
+    </>
+  )
+}
